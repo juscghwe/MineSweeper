4aca52968542b65736e5802a2b6a0388a54f3972
JulianSchwe <62502225+juscghwe@users.noreply.github.com>
2024-09-25 12:17:47 +0200

    added a field vector class to pass around with better readability

---
+++ include/fieldvector.hpp
@@ -0,0 +1,51 @@
+/**
+ * @file fieldvector.hpp
+ * @author juscghwe <a href = "https://github.com/juscghwe">GitHub<\a>
+ * @headerfile "include/cellstruct.hpp"
+ */
+
+#ifndef FIELD_VECTOR_H
+#define FIELD_VECTOR_H
+
+#include "cellstruct.hpp"
+#include <stdexcept>
+#include <vector>
+
+class FieldVector
+{
+  public:
+    FieldVector(std::size_t rows, std::size_t columns, std::size_t mines);
+
+    /**
+     * @brief Get the entire cell at a specific point in the grid.
+     * @param row `size_t` Row the cell is in.
+     * @param col `size_t` Column the cell is in.
+     * @return `CellStructure`
+     * @throws `std::out_of_range` error if the vector is empty.
+     * @throws `std::out_of_range` error if the pointed at cell is out of bounds.
+     */
+    CellStructure& at(size_t row, size_t column) {}
+
+    size_t rows() const {}
+    size_t columns() const {}
+
+  private:
+    std::size_t rows_;
+    std::size_t columns_;
+    std::size_t mines_;
+    std::vector<std::vector<CellStructure>> fieldGrid_;
+
+    /**
+     * @private
+     * @return `bool`
+     */
+    bool isEmpty() {}
+
+    /**
+     * @private
+     * @return `bool`
+     */
+    bool isInvalidIndex(size_t row, size_t column) {}
+};
+
+#endif  // FIELD_VECTOR_H_
---
+++ src/fieldvector.cpp
@@ -0,0 +1,52 @@
+/**
+ * @file fieldvector.cpp
+ * @author juscghwe <a href = "https://github.com/juscghwe">GitHub<\a>
+ * @headerfile "include/fieldvector.hpp"
+ */
+
+#include "fieldvector.hpp"
+
+FieldVector::FieldVector(std::size_t rows, std::size_t columns, std::size_t mines)
+    : rows_(rows), columns_(columns), mines_(mines)
+{}
+
+CellStructure& FieldVector::at(size_t row, size_t column)
+{
+    if (isEmpty) {
+        throw std::out_of_range("Grid not initialized.\n");
+    }
+    if (isInvalidIndex(row, column)) {
+        throw std::out_of_range("Index out of bounds.\n");
+    }
+    return fieldGrid_[row][column];
+}
+
+size_t FieldVector::rows() const
+{
+    if (isEmpty) {
+        return 0;
+    } else {
+        return fieldGrid_.size();
+    }
+}
+
+size_t FieldVector::columns() const
+{
+    if (isEmpty) {
+        return 0;
+    } else {
+        return fieldGrid_[0].size();
+    }
+}
+
+// @private
+bool FieldVector::isEmpty()
+{
+    return fieldGrid_.empty();
+}
+
+// @private
+bool FieldVector::isInvalidIndex(size_t row, size_t column)
+{
+    return (row >= rows() || column >= columns());
+}
