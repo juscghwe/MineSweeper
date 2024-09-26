b7f361496b538d12fb963f6a2d2ee30b6f6ffaee
JulianSchwe <62502225+juscghwe@users.noreply.github.com>
2024-09-26 12:47:05 +0200

    updated cmake build info

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include "fieldgenerator.hpp"
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
 int main()
 {
+    // generate playfield
     FieldGenerator fieldGenerator(10, 10, 4);
     std::unique_ptr<FieldVector> playfield = fieldGenerator.generateField();
     for (std::vector<CellStructure> row : playfield->getFieldGrid()) {
@@ -11,5 +13,23 @@ int main()
         }
         std::cout << "\n";
     }
+
+    // setting up SFML (according to https://github.com/SFML/cmake-sfml-project/blob/a9b227e6575ba2509139208c2ca31f9b54004b40/src/main.cpp)
+    auto window = sf::RenderWindow{{500u, 500u}, "MineSweeper"};
+    window.setFramerateLimit(60);
+    while (window.isOpen()) {
+        //event handling
+        for (auto event = sf::Event{}; window.pollEvent(event);) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        // refresh GUI
+        window.clear();
+        window.display();
+    }
+
+    // exit program
     return 0;
 }
