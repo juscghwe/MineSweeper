eb86a9fbd7cb6fba05901ed957349dbd8078ed0f
JulianSchwe <62502225+juscghwe@users.noreply.github.com>
2024-10-02 22:16:00 +0200

    Added documentation to onecell

--- libs/gui/include/gui/fieldarray.hpp
+++ libs/gui/include/gui/fieldarray.hpp
@@ -1,4 +1,35 @@
 /**
- * @file onecell.hpp
+ * @file fieldarray.hpp
  * @author juscghwe <a href = "https://github.com/juscghwe">GitHub<\a>
  */
+
+#ifndef FIELD_ARRAY_H
+#define FIELD_ARRAY_H
+
+#include "gui/texturemanager.hpp"
+#include "utility/fieldvector.hpp"
+#include <memory>
+
+namespace Gui {
+
+class FieldArray
+{
+  public:
+    FieldArray(const std::unique_ptr<Utility::FieldVector>& kFieldVector, TextureManager& textureManager);
+
+    const std::pair<int, int> getWindowSize() const { return std::pair<int, int>{kWindowX_, kWindowY_}; };
+
+  private:
+    const int kWindowX_;
+    const int kWindowY_;
+    const int kButtonX_;
+    const int kButtonY_;
+    const std::unique_ptr<Utility::FieldVector>& kFieldVector_;
+    TextureManager& textureManager_;
+
+    void initializeGrid();
+};
+
+};  // namespace Gui
+
+#endif
--- libs/gui/include/gui/onecell.hpp
+++ libs/gui/include/gui/onecell.hpp
@@ -1,6 +1,7 @@
 /**
  * @file onecell.hpp
  * @author juscghwe <a href = "https://github.com/juscghwe">GitHub<\a>
+ * @brief This file declares the `OneCell` class which represents a single cell in the Minesweeper grid.
  */
 
 #ifndef ONE_CELL_H
@@ -12,28 +13,55 @@
 
 namespace Gui {
 
+/**
+ * @struct PixelStruct
+ * @brief A simple structure holding the X and Y pixel positions for the cell.
+ */
 struct PixelStruct {
-    int X;
-    int Y;
+    int X;  ///< X position of the cell in pixels
+    int Y;  ///< Y position of the cell in pixels
 };
 
+/**
+ * @class OneCell
+ * @brief Represents a single cell in the Minesweeper game grid, managing its state and visual representation.
+ */
 class OneCell
 {
   public:
+    /**
+     * @brief Constructs a `OneCell` object.
+     * @param cellStruct `Utility::CellStruct` The structural data of the cell (covered, flagged, adjacent mines, etc.).
+     * @param kPosition `PixelStruct` The pixel position of the cell on the screen.
+     * @param textureManager `TextureManager&` Reference to the texture manager responsible for managing cell textures.
+     */
     OneCell(Utility::CellStruct cellStruct, const PixelStruct kPosition, TextureManager& textureManager);
 
+    /**
+     * @brief Marks the cell as clicked, changing its internal state and updating its visual representation.
+     */
     void gotClicked();
 
+    /**
+     * @brief Marks the cell as flagged, changing its internal state and updating its visual representation.
+     */
     void gotFlagged();
 
   private:
-    sf::Sprite sprite_;
-    Utility::CellStruct cellStruct_;
-    const PixelStruct kPosition_;
-    TextureManager& textureManager_;
+    sf::Sprite sprite_;               ///< The sprite representing the cell's texture.
+    Utility::CellStruct cellStruct_;  ///< Data structure containing the cell's logical state.
+    const PixelStruct kPosition_;     ///< The pixel position of the cell.
+    TextureManager& textureManager_;  ///< Reference to the texture manager.
 
+    /**
+     * @brief Retrieves the state of the cell from its structure.
+     * @return `const std::pair<GuiC::CellState, int>` A pair containing the cell's visual state and the number of adjacent mines.
+     */
     const std::pair<GuiC::CellState, int> getStateFromStruct();
 
+    /**
+     * @brief Redraws the cell by updating its sprite texture according to its current state.
+     */
     void redraw();
 };
 
